3063a400bff633f5ebf98a8573170742fe314b2b
Ivan Jacob Agaloos Pesigan <jeksterslab@users.noreply.github.com>
2023-06-16 21:26:36 +0800

    Automated build.

--- src/setup.sh
+++ src/setup.sh
@@ -57,11 +57,11 @@ R -e "tinytex::install_tinytex( \
 )"
 
 ## build details
-echo "$(git ls-remote https://github.com/eddelbuettel/r2u.git master)" > /etc/profile.d/container_init.sh
+echo "$(git ls-remote https://github.com/rocker-org/r2u.git master)" > /etc/profile.d/container_init.sh
 awk '{print $1 > "/etc/profile.d/container_init.sh"}' /etc/profile.d/container_init.sh
 CONTAINER_RELEASE=$(cat /etc/profile.d/container_init.sh)
 echo "export CONTAINER_RELEASE=$CONTAINER_RELEASE" > /etc/profile.d/container_init.sh
-CONTAINER_RELEASE_MSG="\"This release is based on the commit $CONTAINER_RELEASE from the master branch of eddelbuettel/r2u.\""
+CONTAINER_RELEASE_MSG="\"This release is based on the commit $CONTAINER_RELEASE from the master branch of rocker-org/r2u.\""
 echo "export CONTAINER_RELEASE_MSG=$CONTAINER_RELEASE_MSG" >> /etc/profile.d/container_init.sh
 mkdir -p /srv/build
 cd /srv/build
