cff57684fb90e758ecaa01f8518eb35757dc2e4f
jeksterslab <learn.jeksterslab@gmail.com>
2023-06-18 01:29:12 +0000

    Automated build.

--- src/setup.sh
+++ src/setup.sh
@@ -57,19 +57,18 @@ R -e "tinytex::install_tinytex( \
 )"
 
 ## build details
-echo "$(git ls-remote https://github.com/jeksterslab/docker-r2u.git main)" > /etc/profile.d/container_init.sh
-awk '{print $1 > "/etc/profile.d/container_init.sh"}' /etc/profile.d/container_init.sh
-CONTAINER_RELEASE=$(cat /etc/profile.d/container_init.sh)
-echo "export CONTAINER_RELEASE=$CONTAINER_RELEASE" > /etc/profile.d/container_init.sh
-CONTAINER_RELEASE_MSG="\"This release is based on the commit $CONTAINER_RELEASE.\""
-echo "export CONTAINER_RELEASE_MSG=$CONTAINER_RELEASE_MSG" >> /etc/profile.d/container_init.sh
 mkdir -p /srv/build
 cd /srv/build
 touch CONTAINER_RELEASE_MSG
 touch CONTAINER_RELEASE
-echo "$CONTAINER_RELEASE_MSG" > CONTAINER_RELEASE_MSG
-sed -i s/\"//g CONTAINER_RELEASE_MSG
-echo "$CONTAINER_RELEASE" > CONTAINER_RELEASE
+touch CONTAINER_AUTHOR
+echo "$(git ls-remote https://github.com/jeksterslab/docker-r2u.git main)" > /srv/build/CONTAINER_RELEASE
+awk '{print $1 > "/srv/build/CONTAINER_RELEASE"}' /srv/build/CONTAINER_RELEASE
+CONTAINER_RELEASE=$(cat /srv/build/CONTAINER_RELEASE)
+echo "$CONTAINER_RELEASE" > /srv/build/CONTAINER_RELEASE
+CONTAINER_RELEASE_MSG="\"This release is based on the commit $CONTAINER_RELEASE.\""
+echo "$CONTAINER_RELEASE_MSG" > /srv/build/CONTAINER_RELEASE_MSG
+echo "Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>" > /srv/build/CONTAINER_AUTHOR
 
 # Clean up
 rm -rf /var/lib/apt/lists/*
