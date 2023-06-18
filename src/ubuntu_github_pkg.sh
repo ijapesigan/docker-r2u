39e003ad52a600df6bae06b6b3ee933f9dd662d1
Ivan Jacob Agaloos Pesigan <jeksterslab@users.noreply.github.com>
2023-06-18 11:31:27 +0800

    Automated build.

--- src/ubuntu_github_pkg.sh
+++ src/ubuntu_github_pkg.sh
@@ -2,15 +2,17 @@
 
 set -e
 
-# development packages from GitHub
-installGithub.r                   \
-  "rstudio/tinytex"               \
-  "r-lib/cli"                     \
-  "r-lib/devtools"                \
-  "r-hub/rhub"                    \                    
-  "jeksterslab/rProject"          \
-  "jeksterslab/semmcci"           \
-  "jeksterslab/betaDelta"         \
-  "jeksterslab/betaSandwich"      \
-  "jeksterslab/betaNB"            \
-  "jeksterslab/betaMC"
+R -e "remotes::install_github(      \
+    c(                              \
+        'rstudio/tinytex',          \
+        'r-lib/cli',                \
+        'r-lib/devtools',           \
+        'r-hub/rhub',               \
+        'jeksterslab/rProject',     \
+        'jeksterslab/semmcci',      \
+        'jeksterslab/betaDelta',    \
+        'jeksterslab/betaSandwich', \
+        'jeksterslab/betaNB',       \
+        'jeksterslab/betaMC'        \
+    )                               \
+)"
