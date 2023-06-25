d0ce226126af563cbfd5c396bf214aa036cbea0d
ijapesigan <137515526+ijapesigan@users.noreply.github.com>
2023-06-25 23:36:38 +0800

    Update custom.sh

--- tools/custom.sh
+++ tools/custom.sh
@@ -9,7 +9,7 @@ DOCKER_HUB_USERNAME=ijapesigan
 DOCKER_HUB_CONTAINER=r2u
 
 # container
-TEMP_VAR="$(git ls-remote https://github.com/$GIT_HUB_USERNAME/$REPO.git $GIT_HUB_BRANCH)"
+TEMP_VAR=$(git ls-remote https://github.com/"$GIT_HUB_USERNAME"/"$GIT_HUB_REPO".git "$GIT_HUB_BRANCH")
 echo "$TEMP_VAR" > /etc/profile.d/container_init.sh
 awk '{print $1 > "/etc/profile.d/container_init.sh"}' /etc/profile.d/container_init.sh
 CONTAINER_RELEASE=$(cat /etc/profile.d/container_init.sh)
@@ -17,14 +17,14 @@ echo "export CONTAINER_RELEASE=$CONTAINER_RELEASE" > /etc/profile.d/container_in
 CONTAINER_RELEASE_MSG="\"This release is based on commit $CONTAINER_RELEASE.\""
 echo "export CONTAINER_RELEASE_MSG=$CONTAINER_RELEASE_MSG" >> /etc/profile.d/container_init.sh
 
-# github
-echo "export GIT_HUB_USERNAME=$GIT_HUB_USERNAME" >> /etc/profile.d/container_init.sh
-echo "export GIT_HUB_REPO=$GIT_HUB_REPO" >> /etc/profile.d/container_init.sh
-echo "export GIT_HUB_BRANCH=$GIT_HUB_BRANCH" >> /etc/profile.d/container_init.sh
 
-# docker
-echo "export DOCKER_HUB_USERNAME=$DOCKER_HUB_USERNAME" >> /etc/profile.d/container_init.sh
-echo "export DOCKER_HUB_CONTAINER=$DOCKER_HUB_CONTAINER" >> /etc/profile.d/container_init.sh
+{
+  echo "export GIT_HUB_USERNAME=$GIT_HUB_USERNAME"
+  echo "export GIT_HUB_REPO=$GIT_HUB_REPO"
+  echo "export GIT_HUB_BRANCH=$GIT_HUB_BRANCH"
+  echo "export DOCKER_HUB_USERNAME=$DOCKER_HUB_USERNAME"
+  echo "export DOCKER_HUB_CONTAINER=$DOCKER_HUB_CONTAINER"
+} >> /etc/profile.d/container_init.sh
 
 # srv
 mkdir -p /srv/build
