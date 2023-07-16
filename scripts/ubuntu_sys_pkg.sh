e0b4503147e4a619d1612beb3c3f07673afd4326
ijapesigan <ijapesigan@gmail.com>
2023-07-16 15:40:41 +0000

    Automated build.

--- scripts/ubuntu_sys_pkg.sh
+++ scripts/ubuntu_sys_pkg.sh
@@ -50,3 +50,14 @@ rm -rf quarto*
 wget https://raw.githubusercontent.com/rocker-org/rocker-versioned2/master/scripts/install_pandoc.sh
 bash install_pandoc.sh
 rm -rf install_pandoc.sh
+
+# gh cli
+apt_install        \
+    golang-go      \
+    curl
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt update
+apt install gh -y
