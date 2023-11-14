603e0859d873bad8f4e11169a8f841077e65377f
ijapesigan <ijapesigan@gmail.com>
2023-11-14 21:20:37 +0000

    Automated build.

--- scripts/openmx.sh
+++ scripts/openmx.sh
@@ -3,7 +3,7 @@
 set -e
 
 # OpenMx dependencies
-install2.r --error --skipmissing --skipinstalled -n -1 \
+install2.r --error --skipinstalled -n -1 \
     BH           \
     covr         \
     digest       \
